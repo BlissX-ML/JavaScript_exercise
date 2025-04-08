61601b3852dce400deafa60589cae0170a8df47c
Tong0204-ning <liumotong95@gmail.com>
2025-04-08 16:03:21 +0800

    Achieve click event and click function

--- passenger_counter_app/counter.js
+++ passenger_counter_app/counter.js
@@ -1,1 +1,12 @@
 // document.getElementById('count_number').innerText = 5
+
+let count = 0;
+
+let display_num = document.getElementById('count_number');
+let plus_btn = document.getElementById('plus-btn');
+
+function increment() {
+    count += 1;
+    display_num.innerText = count;
+}
+
