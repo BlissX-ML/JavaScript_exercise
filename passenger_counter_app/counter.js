7b161a7a7947c2ccaea65cb530c3eccbf617127f
Tong0204-ning <liumotong95@gmail.com>
2025-04-09 09:38:45 +0800

    Complete Counter App

--- passenger_counter_app/counter.js
+++ passenger_counter_app/counter.js
@@ -1,12 +1,23 @@
-// document.getElementById('count_number').innerText = 5
+let countnumber = document.getElementById('count_number')
+let plusbtn = document.getElementById('plus-btn');
+
+let savebtn = document.getElementById('save-btn');
+let saveEL = document.getElementById('save-el');
 
 let count = 0;
 
-let display_num = document.getElementById('count_number');
-let plus_btn = document.getElementById('plus-btn');
-
 function increment() {
     count += 1;
-    display_num.innerText = count;
+    if (count > 20) {
+        count -= 1;
+        alert('Please, less than 20.');
+    }
+    countnumber.textContent = count;
 }
 
+function save() {
+    let saveString = ' ' + count + ' -';
+    saveEL.textContent += saveString;
+    countnumber.textContent = 0;
+    count = 0;
+}
